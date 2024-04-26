9a9637e1ca48f08f426198a75c61831c769de953
Lucifer <luciferxhnh@gmail.com>
2024-04-26 09:26:37 +0700

    m

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import './App.css';
-import { useState } from 'react';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
   const [formattedNum, setFormattedNum] = useState("");
 
   const formatNum = () => {
     var licenseNum = document.querySelector("#licenseNum").value;
-    console.log(formatBienSo(licenseNum))
-    setFormattedNum(formatBienSo(licenseNum))
-  }
+    console.log(formatBienSo(licenseNum));
+    setFormattedNum(formatBienSo(licenseNum));
+  };
 
   function formatBienSo(bienSo) {
     var firstPart = "";
     var secondPart = "";
     var hyphen = "-";
-  
+
     try {
       var trimmedBienSo = bienSo.trim();
       for (var i = 0; i < trimmedBienSo.length; i++) {
@@ -22,18 +22,16 @@ function App() {
         if (isAlphaNumeric(char) && firstPart.length < 4) {
           firstPart += char;
         } else {
-
           if (isNumeric(char) && secondPart.length < 5) {
             secondPart += char;
           }
         }
       }
-      
-      if (trueLicenseNum(firstPart.toUpperCase() + hyphen + secondPart)){
-        return firstPart.toUpperCase() + hyphen + secondPart
+
+      if (trueLicenseNum(firstPart.toUpperCase() + hyphen + secondPart)) {
+        return firstPart.toUpperCase() + hyphen + secondPart;
       }
       return "Không thể format. Liên hệ dev để biết thêm chi tiết";
-  
     } catch (error) {
       return bienSo;
     }
@@ -42,7 +40,7 @@ function App() {
   function isNumeric(char) {
     return !isNaN(parseFloat(char)) && isFinite(char);
   }
-  
+
   function isAlphaNumeric(char) {
     return /^[a-zA-Z0-9]*$/.test(char);
   }
@@ -52,20 +50,39 @@ function App() {
     return regex.test(chuoi);
   }
 
+setInterval(function() {
+  fetchData(); 
+}, 15 * 60 * 1000);
+
+function fetchData() {
+  fetch("https://lucifer-dep-trai-d0efa5f23a33.herokuapp.com/helloworld")
+    .then(response => {
+      if (!response.ok) {
+        throw new Error("Yêu cầu không thành công");
+      }
+      return response.text();
+    })
+    .then(data => {
+      console.log(data);
+    })
+    .catch(error => {
+      console.error("Lỗi: ", error);
+    });
+}
+
+
   return (
     <>
-      <div style={{padding:"0 15px"}}>
+      <div style={{ padding: "0 15px" }}>
         <h2>Format lisense number by Lucifer</h2>
-        <div>
-          Nhập biển số máy:
-        </div>
+        <div>Nhập biển số máy:</div>
         <span>
-          <input id="licenseNum" type="text"/>
+          <input id="licenseNum" type="text" />
           <button onClick={formatNum}>Format</button>
         </span>
-        <div style={{marginTop: "15px"}}>
+        <div style={{ marginTop: "15px" }}>
           Biển số xe của bạn là:
-          <span style={{color: "red", marginLeft: "10px"}}>
+          <span style={{ color: "red", marginLeft: "10px" }}>
             {formattedNum}
           </span>
         </div>
